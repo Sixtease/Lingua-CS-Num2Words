10bf0450d711eeae1982fb4c986497889c971722
Roman Vašíček <rvasicek@cpan.org>
2004-06-10 11:53:54 +0000

    Import of RVASICEK/Lingua-CS-Num2Word-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-CS-Num2Word
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         RVASICEK/Lingua-CS-Num2Word-0.02.tar.gz
    gitpan-cpan-author:       RVASICEK
    gitpan-cpan-maturity:     released

--- Num2Word.pm
+++ Num2Word.pm
@@ -1,22 +1,32 @@
-# For Emacs: -*- mode:cperl; mode:folding; coding:iso-8859-2 -*-
+# For Emacs: -*- mode:cperl; mode:folding; coding:iso-8859-2; -*-
 #
-# Started by rv@petamem.com at 2002-07-01
+# (c) 2002-2004 PetaMem, s.r.o.
 #
-# $Id: Num2Word.pm,v 1.11 2002/07/12 14:06:53 rv Exp $
+# PPCG: 0.7
 #
-# PPCG: 0.5
 
 package Lingua::CS::Num2Word;
 
+# {{{ use block
+
 use strict;
+
+# }}}
+
+# {{{ BEGIN
 
 BEGIN {
   use Exporter ();
-  use vars qw($VERSION @ISA @EXPORT_OK);
-  $VERSION = '0.01';
-  @ISA     = qw(Exporter);
-  @EXPORT_OK = qw(&num2cs_cardinal);
+  use vars qw($VERSION $REVISION @ISA @EXPORT_OK);
+  $VERSION    = 0.02;
+  ($REVISION) = '$Revision: 1.14 $' =~ /([\d.]+)/;
+  @ISA        = qw(Exporter);
+  @EXPORT_OK  = qw(&num2cs_cardinal);
 }
+
+# }}}
+
+# {{{ variables
 
 my %token1 = qw( 0 nula         1 jedna         2 dva
                  3 tøi          4 ètyøi         5 pìt
@@ -35,7 +45,9 @@ my %token3 = (  100, 'sto', 200, 'dvì stì',   300, 'tøi sta',
                 700, 'sedm set',  800, 'osm set',   900, 'devìt set'
 	     );
 
-# {{{ num2cs_cardinal                 number to string conversion
+# }}}
+
+# {{{ num2cs_cardinal           number to string conversion
 
 sub num2cs_cardinal {
   my $result = '';
@@ -55,26 +67,27 @@ sub num2cs_cardinal {
     } else {
       $result = $token2{$number - $reminder}.' '.num2cs_cardinal($reminder);
     }
-  } elsif ($number < 1000) {
+  } elsif ($number < 1_000) {
     $reminder = $number % 100;
     if ($reminder != 0) {
       $result = $token3{$number - $reminder}.' '.num2cs_cardinal($reminder);
     } else {
       $result = $token3{$number};
     }
-  } elsif ($number < 1000000) {
-    $reminder = $number % 1000;
+  } elsif ($number < 1_000_000) {
+    $reminder = $number % 1_000;
     my $tmp1 = ($reminder != 0) ? ' '.num2cs_cardinal($reminder) : '';
     my $tmp2 = substr($number, 0, length($number)-3);
-    my $tmp3 = $tmp2 % 10;
+    my $tmp3 = $tmp2 % 100;
+    my $tmp4 = $tmp2 % 10;
 
-    if ($tmp2 < 9 || $tmp2 > 20 && $tmp2 < 109 || $tmp2 > 120) {
+    if ($tmp3 < 9 || $tmp3 > 20) {
 
-      if ($tmp3 == 1 && $tmp2 == 1) {
+      if ($tmp4 == 1 && $tmp2 == 1) {
 	$tmp2 = 'tisíc';
-      } elsif ($tmp3 == 1) {
-	$tmp2 = num2cs_cardinal($tmp2 - $tmp3).' jeden tisíc';
-      } elsif($tmp3 > 1 && $tmp3 < 5) {
+      } elsif ($tmp4 == 1) {
+	$tmp2 = num2cs_cardinal($tmp2 - $tmp4).' jeden tisíc';
+      } elsif($tmp4 > 1 && $tmp4 < 5) {
 	$tmp2 = num2cs_cardinal($tmp2).' tisíce';
       } else {
 	$tmp2 = num2cs_cardinal($tmp2).' tisíc';
@@ -85,19 +98,20 @@ sub num2cs_cardinal {
 
     $result = $tmp2.$tmp1;
 
-  } elsif ($number < 1000000000) {
-    $reminder = $number % 1000000;
+  } elsif ($number < 1_000_000_000) {
+    $reminder = $number % 1_000_000;
     my $tmp1 = ($reminder != 0) ? ' '.num2cs_cardinal($reminder) : '';
     my $tmp2 = substr($number, 0, length($number)-6);
-    my $tmp3 = $tmp2 % 10;
+    my $tmp3 = $tmp2 % 100;
+    my $tmp4 = $tmp2 % 10;
 
-    if ($tmp2 < 9 || $tmp2 > 20 && $tmp2 < 109 || $tmp2 > 120) {
+    if ($tmp3 < 9 || $tmp3 > 20) {
 
-      if ($tmp3 == 1 && $tmp2 == 1) {
+      if ($tmp4 == 1 && $tmp2 == 1) {
 	$tmp2 = 'milion';
-      } elsif ($tmp3 == 1) {
-	$tmp2 = num2cs_cardinal($tmp2 - $tmp3).' jeden milion';
-      } elsif($tmp3 > 1 && $tmp3 < 5) {
+      } elsif ($tmp4 == 1) {
+	$tmp2 = num2cs_cardinal($tmp2 - $tmp4).' jeden milion';
+      } elsif($tmp4 > 1 && $tmp4 < 5) {
 	$tmp2 = num2cs_cardinal($tmp2).' miliony';
       } else {
 	$tmp2 = num2cs_cardinal($tmp2).' milionù';
@@ -116,7 +130,6 @@ sub num2cs_cardinal {
 }
 
 # }}}
-
 
 1;
 __END__
@@ -155,18 +168,21 @@ Convert number to text representation.
 
 num2cs_cardinal
 
+=head1 KNOWN BUGS
+
+None.
+
 =head1 AUTHOR
 
 Roman Vasicek E<lt>rv@petamem.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 PetaMem s.r.o.
+Copyright (c) 2002-2004 PetaMem s.r.o.
 
-This package is free software. Tou can redistribute and/or modify it under
+This package is free software. You can redistribute and/or modify it under
 the same terms as Perl itself.
 
 =cut
 
 # }}}
-
