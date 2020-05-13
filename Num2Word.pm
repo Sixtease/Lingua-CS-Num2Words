12b259914f53368078c45a2a9fb4cd79dd3bc801
Jan Oldřich Krůza <jan@sixtease.net>
2020-05-13 10:45:35 +0200

    indent

--- Num2Word.pm
+++ Num2Word.pm
@@ -31,22 +31,27 @@ BEGIN {
 
 # {{{ variables
 
-my %token1 = qw( 0 nula         1 jedna         2 dva
-                 3 tři          4 čtyři         5 pět
-                 6 šest         7 sedm          8 osm
-                 9 devět        10 deset        11 jedenáct
-                 12 dvanáct     13 třináct      14 čtrnáct
-                 15 patnáct     16 šestnáct     17 sedmnáct
-                 18 osmnáct     19 devatenáct
-               );
-my %token2 = qw( 20 dvacet      30 třicet       40 čtyřicet
-                 50 padesát     60 šedesát      70 sedmdesát
-                 80 osmdesát    90 devadesát
-               );
-my %token3 = (  100, 'sto', 200, 'dvě stě',   300, 'tři sta',
-                400, 'čtyři sta', 500, 'pět set',   600, 'šest set',
-                700, 'sedm set',  800, 'osm set',   900, 'devět set'
-	     );
+my %token1 = qw(
+   0 nula         1 jedna         2 dva
+   3 tři          4 čtyři         5 pět
+   6 šest         7 sedm          8 osm
+   9 devět       10 deset        11 jedenáct
+  12 dvanáct     13 třináct      14 čtrnáct
+  15 patnáct     16 šestnáct     17 sedmnáct
+  18 osmnáct     19 devatenáct
+);
+
+my %token2 = qw(
+  20 dvacet      30 třicet       40 čtyřicet
+  50 padesát     60 šedesát      70 sedmdesát
+  80 osmdesát    90 devadesát
+);
+
+my %token3 = (
+  100, 'sto',       200, 'dvě stě',   300, 'tři sta',
+  400, 'čtyři sta', 500, 'pět set',   600, 'šest set',
+  700, 'sedm set',  800, 'osm set',   900, 'devět set'
+);
 
 # }}}
 
